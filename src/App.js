7a1da06dea3b21aa5020a9be5be6440422e458c5
Dennis <dennissung6105@gmail.com>
2025-02-24 20:53:23 +1000

    API for searching up the pokemon, then displaying them as image has been implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+// return the json of the pokemon that was searched
+async function fetchPokemon(pokemon) {
+  console.log(pokemon.get("query"))
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemon.get("query")}/`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.error(error.message);
+  }
+
+}
+
 function App() {
+  const [pokemonData, setPokemonData] = useState(null);
+  const [error, setError] = useState(null);
+
+  // Handle Form Submission
+  const handleSubmit = async (event) => {
+    event.preventDefault(); 
+
+    const formData = new FormData(event.target)
+    const data = await fetchPokemon(formData)
+
+    if (data) {
+      setPokemonData(data);
+      setError(null);
+    } else {
+      setError("Pokemon not found!");
+      setPokemonData(null);
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input name="query"/>
+      </form>
+
+      {/* If error occur, display error */}
+      {error && <p style={{color: 'red'}}>{error}</p>}
+
+      {/* Display pokemon if no error occurred */}
+      {pokemonData && (
+        <div>
+          <img src={pokemonData.sprites.front_default}/>
+        </div>
+      )}
+    </>
   );
 }
 
